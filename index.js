1d56569ce55e0fb2cc1b0a87ce17fd3b2ae149a6
SFlorence96 <safarri.florence@gmail.com>
2025-01-26 17:19:25 +0000

    editng code

---
+++ index.js
@@ -0,0 +1,21 @@
+let post = [ 
+  { id : 1 , title : "Blog Post 1" , authors : "Author A" , date : "2023-11-15" , src: "https://placehold.co/300x300"} ,  
+  { id : 2 , title : "Blog Post 2" , authors : "Author B" , date : "2023-12-01" , src: "https://placehold.co/300x300"},  
+] ; 
+
+let section = document.getElementById("section") ; 
+
+for (let i = 0 ; i < post.length ; i ++)  
+{  let bootstrapCard = 
+<div class="card" style="width: 18rem;">
+  <img src="${post[i].src}" class="card-img-top" alt="This is alt text">
+  <div class="card-body">
+    <h5 class="card-title">${post[i].title}</h5>
+    <p class="card-text">${post[i].date}</p>
+    <p class="card-text">${post[i].authors}</p>
+  </div> 
+</div>  ;   
+}
+
+
+section.innerHTML += bootstrapCard ; 
